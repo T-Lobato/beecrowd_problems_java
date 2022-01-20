8a409450957dcc1f9e84c1bc0978ac862d2f8638
Thyago Lobato <thyagollobato@gmail.com>
2022-01-20 20:15:38 -0300

    Média 1

---
+++ JAVA/Ex1005.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex1005 { //Alterar o nome da classe para Main ara funcionar no beecrowd
+
+    public static void main(String[] args) {
+        
+        double nota1, nota2, media;
+
+        Scanner sc = new Scanner(System.in);
+
+        nota1 = sc.nextDouble() * 3.5;
+        nota2 = sc.nextDouble() * 7.5;
+
+        media = (nota1 + nota2) / 11;
+
+        System.out.printf("MEDIA = %.5f\n", media);
+
+        sc.close();     
+        
+    }
+    
+}
