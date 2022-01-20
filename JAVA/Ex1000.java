3f71f22def27b981035ec6a7047e6e022cbd9c79
Lobatonn <thyagollobato@gmail.com>
2022-01-19 22:28:53 -0300

    Initial Commit

---
+++ JAVA/Ex1000.java
@@ -0,0 +1,13 @@
+public class Ex1000 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+
+       
+    }
+
+
+
+}
---
+++ JAVA/Ex1001.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex1001 {
+
+    public static void main(String[] args) {
+        
+        int a,b;
+
+        int resultado;
+
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        resultado = a + b;
+
+        System.out.println("X = " + resultado);
+
+
+    }
+    
+}
