9f503d92a1b119d5542afa9a78ebac79d91bd5b3
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 10:34:39 -0300

    Extremamente Básico

--- JAVA/Ex1001.java
+++ JAVA/Ex1001.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Ex1001 { // Alterar o nome da classe para Main ara funcionar no beecrowd.
 
     public static void main(String[] args) {
-        
+        // variaveis
         int a,b;
 
         int resultado;
 
         Scanner sc = new Scanner(System.in);
 
+        //entrada
         a = sc.nextInt();
         b = sc.nextInt();
-
+        
+        //processamento
         resultado = a + b;
 
+        //saida
         System.out.println("X = " + resultado);
-
         sc.close();
     }
-    
 }
