27d76eccda036b79a0bf75b6be270988ffad89c6
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 17:39:31 -0300

    Salário com Bônus

---
+++ JAVA/Ex1009.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex1009 { //Alterar o nome da classe para Main para funcionar no beecrowd
+
+    public static void main(String[] args) {
+        
+        //variaveis
+        String nome; 
+        double salarioFixo, vendas, totalGeral, taxa = 0.15;
+
+        Scanner sc = new Scanner(System.in);
+
+        //entrada    
+        nome = sc.next();
+        salarioFixo = sc.nextDouble();
+        vendas = sc.nextDouble();
+
+        //processamento
+        totalGeral = vendas * taxa + salarioFixo;
+
+        //saida
+        System.out.printf("TOTAL = R$ %.2f\n", totalGeral);
+        sc.close();      
+    }  
+}
