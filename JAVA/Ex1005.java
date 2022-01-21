219f595f1fea7f8a8a9c48afc85a23f9dc830e1b
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 10:56:21 -0300

    Média 1

--- JAVA/Ex1005.java
+++ JAVA/Ex1005.java
@@ -4,19 +4,21 @@ public class Ex1005 { //Alterar o nome da classe para Main para funcionar no bee
 
     public static void main(String[] args) {
         
+        //variaveis
         double nota1,nota2,media;
 
         Scanner sc = new Scanner(System.in);
 
+        //entrada
         nota1 = sc.nextDouble() * 3.5;
         nota2 = sc.nextDouble() * 7.5;
 
+        //processamento
         media = (nota1 + nota2) / 11;
 
+        //saida
         System.out.printf("MEDIA = %.5f\n", media);
-
         sc.close();     
         
     }
-    
 }
