a65755ab1372e88f7a247f2519d6c991653c20a0
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 10:51:45 -0300

    Soma Simples

--- JAVA/Ex1003.java
+++ JAVA/Ex1003.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
-public class Ex1003 {
+public class Ex1003 { // Alterar o nome da classe para Main para funcionar no beecrowd.
     
     public static void main(String[] args) {
-
+        
+        //variaveis
         int a,b,resultado;
 
         Scanner sc = new Scanner(System.in);
 
+        //entrada
         a = sc.nextInt();
         b = sc.nextInt();
 
+        //processamento
         resultado = a + b;
 
+        //saida
         System.out.println("SOMA = " + resultado);
-        
         sc.close();
     }
 }
