cbb409775afc5d6440c631c674a629543eee3387
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 10:48:30 -0300

    Área do Círculo

--- JAVA/Ex1002.java
+++ JAVA/Ex1002.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
 
-public class Ex1002{
+public class Ex1002{ // // Alterar o nome da classe para Main para funcionar no beecrowd.
 
 
 public static void main(String args[]) {
-
-        double r,a;
-
-        double n = 3.14159;
-
+        //variaveis
+        double r, a, n = 3.14159;
+        
         Scanner sc = new Scanner(System.in);
-
+        //entrada
         r = sc.nextDouble();
-
+        //processamento
         a = n * Math.pow(r, 2);
-
+        //saida
         System.out.printf("A=%.4f\n", a);
-
         sc.close();    
-
-    
+  
     }
-
 }
