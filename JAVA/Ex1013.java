c3fcbdfe73dc0406bf4cb4cc00f56992a408fb6c
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 23:18:31 -0300

    O Maior

---
+++ JAVA/Ex1013.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex1013 {
+    public static void main(String[] args) {
+        
+        //objeto
+        Scanner sc = new Scanner(System.in);
+
+        //variaveis & entrada
+        String[] valores = sc.nextLine().split(" ");
+        int a = Integer.parseInt(valores[0]);
+        int b = Integer.parseInt(valores[1]);
+        int c = Integer.parseInt(valores[2]);
+
+        //processamento
+        int maiorAB = (a + b + Math.abs(a-b))/2;
+        int maiorABC = (maiorAB + c + Math.abs(maiorAB - c))/2;
+
+        //saida
+        System.out.println(maiorABC + " eh o maior");
+        sc.close();
+    }
+}
