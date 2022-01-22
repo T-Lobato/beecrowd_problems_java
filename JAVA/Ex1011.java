e8aff4955fa859ca1bffdfc821784837243f263b
Thyago Lobato <thyagollobato@gmail.com>
2022-01-21 22:27:15 -0300

    Esfera

--- JAVA/Ex1011.java
+++ JAVA/Ex1011.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Ex1011 {
+public class Ex1011 { //Alterar o nome da classe para Main para funcionar no beecrowd
+
     public static void main(String[] args) {
         
         //variaveis & objetos
