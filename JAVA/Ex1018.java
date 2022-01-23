643fb8a88588510d61e1ccb720ae87df45ed002c
Thyago Lobato <thyagollobato@gmail.com>
2022-01-23 15:37:08 -0300

    Cédulas

--- JAVA/Ex1018.java
+++ JAVA/Ex1018.java
@@ -18,13 +18,14 @@ public class Ex1018 { //Alterar o nome da classe para Main para funcionar no bee
         int um = ((((((n%100)%50)%20)%10)%5)%2)/1;
 
        //saida
-        System.out.println(cem + " notas(s) de R$ 100,00");
-        System.out.println(cinquenta + " notas(s) de R$ 50,00");
-        System.out.println(vinte + " notas(s) de R$ 20,00");
-        System.out.println(dez + " notas(s) de R$ 10,00");
-        System.out.println(cinco + " notas(s) de R$ 5,00");
-        System.out.println(dois + " notas(s) de R$ 2,00");
-        System.out.println(um + " notas(s) de R$ 1,00");
+        System.out.println(n);
+        System.out.println(cem + " nota(s) de R$ 100,00");
+        System.out.println(cinquenta + " nota(s) de R$ 50,00");
+        System.out.println(vinte + " nota(s) de R$ 20,00");
+        System.out.println(dez + " nota(s) de R$ 10,00");
+        System.out.println(cinco + " nota(s) de R$ 5,00");
+        System.out.println(dois + " nota(s) de R$ 2,00");
+        System.out.println(um + " nota(s) de R$ 1,00");
         sc.close();
      }
 }
