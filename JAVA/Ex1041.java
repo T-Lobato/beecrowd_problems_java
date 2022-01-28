83525f61528291160b3d804124f434fc37c1b4f8
Thyago Lobato <thyagollobato@gmail.com>
2022-01-27 22:10:29 -0300

    Coordenadas de um Ponto

---
+++ JAVA/Ex1041.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ex1041 {
+    public static void main(String[] args) {
+
+        //objeto
+        Scanner sc = new Scanner(System.in);
+        String [] valores = sc.nextLine().split(" ");
+
+        //variaveis e entrada
+        double x = Double.parseDouble(valores[0]);
+        double y = Double.parseDouble(valores[1]);
+
+        //variaveis e processamento
+
+        if (x > 0 && y > 0){
+            System.out.println("Q1");
+
+        } else if ( x > 0 && y < 0){
+            System.out.println("Q4");
+
+        } else if (x < 0 && y < 0){
+            System.out.println("Q3");
+
+        } else if (x < 0 && y > 0){
+            System.out.println("Q2");
+
+        } else if (x == 0 && y == 0){
+            System.out.println("Origem");
+
+        } else if (x == 0 && y != 0){
+            System.out.println("Eixo Y");
+
+        } else if (x != 0 && y ==0){
+            System.out.println("Eixo X");
+        }
+    }   
+}
+    
