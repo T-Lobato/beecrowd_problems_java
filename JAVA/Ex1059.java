15104457006542c675ebc7393733796627c40e5f
Thyago Lobato <thyagollobato@gmail.com>
2022-01-31 22:26:26 -0300

    Números Pares

---
+++ JAVA/Ex1059.java
@@ -0,0 +1,5 @@
+public class Ex1059 {// Alterar para main para funcionar no Beecrowd
+    public static void main(String[] args) {
+        for(int i = 2; i < 101; i+=2) System.out.println(i);
+    }
+}
