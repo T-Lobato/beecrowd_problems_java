ed8eed2a8fa36d7dcdfa69f775bfeb6767c8b83f
Thyago Lobato <thyagollobato@gmail.com>
2022-01-31 22:55:58 -0300

    Números Positivo

---
+++ JAVA/Ex1060.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex1060 {// alterar o nome da classe para Main para funcionar no Beecrowd
+    public static void main(String[] args) {
+        //Objeto
+        Scanner sc = new Scanner(System.in);
+        
+        //Variaveis e Entrada
+        double numero;
+        int contador = 0;
+
+        for(int i = 0; i < 6; i++){
+            numero = sc.nextDouble();
+
+           if(numero > 0){
+               contador++;                
+           }
+        }
+        sc.close();
+        System.out.println(contador + " valores positivos");
+    }
+}
+
