3271b2845fdb0620cbc61ee0d9e1fff7340d6d56
Thyago Lobato <thyagollobato@gmail.com>
2022-02-07 20:08:50 -0300

    Seis Números Ímpares

---
+++ JAVA/Ex1068.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex1068 { //Alterar o nome da classe para Main para funcionar no beecrowd
+    public static void main(String[] args) {
+        //Objeto
+        Scanner sc = new Scanner(System.in);
+
+        // Variaveis e Entrada
+        int valorEntrada = sc.nextInt();
+        sc.close();
+
+        //Processamento e saida
+        for(int i = 0; i < 12; i++){
+
+            if(valorEntrada % 2 == 0){
+                valorEntrada++;
+            }
+            else {
+                System.out.println(valorEntrada);
+                valorEntrada++;
+            }
+        }
+    }
+}
