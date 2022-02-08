7e1f88ed7ff541b1ab5b0c5ade7ae12ee4c3fa02
Thyago Lobato <thyagollobato@gmail.com>
2022-02-07 21:30:39 -0300

    Seis Números Ímpares

--- JAVA/Ex1070.java
+++ JAVA/Ex1070.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex1068 { //Alterar o nome da classe para Main para funcionar no beecrowd
+public class Ex1070 { //Alterar o nome da classe para Main para funcionar no beecrowd
     public static void main(String[] args) {
         //Objeto
         Scanner sc = new Scanner(System.in);
