ac36384864965cc039f109c55a200eddff556fb0
Thyago Lobato <thyagollobato@gmail.com>
2022-02-15 19:59:04 -0300

    Tabuada

--- JAVA/Ex1078.java
+++ JAVA/Ex1078.java
@@ -1,16 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Ex1078 { // Alterar o nome da classe para Main para funcionar no beecrowd
+public class Ex1078 { //  Alterar o nome da classe para Main para funcionar no beecrowd
     public static void main(String[] args) throws Exception {
-        // Objeto
+        //  Objeto
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // Variavel e Entrada
+        //  Variavel e Entrada
         int numero = Integer.parseInt(br.readLine());
         br.close();
 
-        // Processamento e Saída
+        //  Processamento e Saída
         for(int i = 1; i < 11; i++) System.out.printf("%d x %d = %d\n", i, numero, i * numero);
     }
 }
