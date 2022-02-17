beb5e12059a2025eeda3978a821b5d8ba8dc02e9
Thyago Lobato <thyagollobato@gmail.com>
2022-02-17 18:50:07 -0300

    Médias Ponderadas

---
+++ JAVA/Ex1079.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex1079 { // Alterar o nome da classe para main para funcionar no beecrowd
+    public static void main(String[] args) {
+        // Objeto
+        Scanner sc = new Scanner(System.in);
+
+        //Variaveis e Entrada
+        int quantLinhas = sc.nextInt();
+        double a, b, c;
+        int aux = 0;
+
+        //Processamento e Saída
+        while(aux < quantLinhas){
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            c = sc.nextDouble();
+            Double result = ((a * 2) + (b * 3) + (c * 5)) / 10;
+            
+            System.out.printf("%.1f\n", result);
+            aux++;
+        }
+        sc.close();
+    }
+}
