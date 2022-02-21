53f672e39fa9b8b4a1d284d00419474fa3762358
Thyago Lobato <thyagollobato@gmail.com>
2022-02-21 20:06:54 -0300

    Sequencia IJ 1

---
+++ JAVA/Ex1095.java
@@ -0,0 +1,7 @@
+public class Ex1095{ // Alterar o nome da classe para Main para funcionar no beecrowd
+public static void main(String[] args) {
+ 
+        // Processamento & Saída
+        for(int i = 1, j = 60; j >= 0; i+=3, j-=5) System.out.printf("I=%d J=%d\n", i, j);
+    }
+}
