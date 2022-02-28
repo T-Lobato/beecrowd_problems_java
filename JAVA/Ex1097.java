6b4f0825bb9c8c8e923cfd76801081880553b270
Thyago Lobato <thyagollobato@gmail.com>
2022-02-28 14:16:03 -0300

    Sequencia IJ 3

---
+++ JAVA/Ex1097.java
@@ -0,0 +1,21 @@
+public class Ex1097{ // Alterar o nome da classe para Main para funcionar no beecrowd
+    public static void main(String[] args) {
+
+        // Processamento & Saída
+        for(int i = 1, j = 7; j > 4; j--) {
+            System.out.printf("I=%d J=%d\n", i, j);
+        }
+        for(int i = 3, j = 9; j > 6; j--) {
+            System.out.printf("I=%d J=%d\n", i, j);
+        }
+        for(int i = 5, j = 11; j > 8; j--) {
+            System.out.printf("I=%d J=%d\n", i, j);
+        }
+        for(int i = 7, j = 13; j > 10; j--) {
+            System.out.printf("I=%d J=%d\n", i, j);
+        }
+        for(int i = 9, j = 15; j > 12; j--) {
+            System.out.printf("I=%d J=%d\n", i, j);
+        }
+    }
+}
