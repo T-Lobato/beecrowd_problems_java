03491e0da23c60d83418da9a85e275be5c1574b0
Thyago Lobato <thyagollobato@gmail.com>
2022-03-05 23:13:38 -0300

    Linha na Matriz

--- JAVA/Ex1181.java
+++ JAVA/Ex1181.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main { // Alterar o nome da classe para Main para funcionar no beecrowd
+public class Ex1181 { // Alterar o nome da classe para Main para funcionar no beecrowd
     public static void main(String[] args) {
 
         // Objeto
