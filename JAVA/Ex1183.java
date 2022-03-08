2b34ae7314b22f18d2e18d7a79936ab3ba19de4b
Thyago Lobato <thyagollobato@gmail.com>
2022-03-08 17:49:32 -0300

    Acima da Diagonal Principal

---
+++ JAVA/Ex1183.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ex1183{ // Alterar o nome da classe para Main para funcionar no Beecrowd
+    public static void main(String[] args) {
+        // Objeto
+        Scanner sc = new Scanner(System.in);
+        float[][] mat = new float[12][12];
+
+        // Variáveis & Entrada
+        String somaOuMedia = sc.nextLine();
+        float soma = 0;
+        int cont = 0;
+
+        // Processamento
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 12; j++) {
+                mat[i][j] = sc.nextFloat();
+                if(j > i) {
+                soma += mat[i][j]; 
+                cont++;
+                }
+            }
+        }
+        sc.close();
+        // Processamento e Saída
+        switch(somaOuMedia) {
+            case "S":
+            System.out.printf("%.1f\n", soma);
+            break;
+            case "M":
+            System.out.printf("%.1f\n", soma / cont);
+            break;
+        }
+    }
+}
