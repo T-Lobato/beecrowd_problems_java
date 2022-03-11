5f46dddd1c9a069eb7ea3aa156b86459c7548f99
Thyago Lobato <thyagollobato@gmail.com>
2022-03-11 17:51:40 -0300

    Sequência S

--- JAVA/Ex1155.java
+++ JAVA/Ex1155.java
@@ -1,9 +1,9 @@
 public class Ex1155 { // Alterar o nome da classe para Main para funcionar no beecrowd
     public static void main(String[] args) {
-        
+
         // Processamento
-        double s = 1.5;
-        for(int i = 3; i < 101; i++) { 
+        double s = 0;
+        for(int i = 1; i < 101; i++) { 
             s +=  1.0/i;
         }
         // Saída
