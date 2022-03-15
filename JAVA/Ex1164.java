83c9f29276251eebd98fba52b1ab5c11d7e9555f
Thyago Lobato <thyagollobato@gmail.com>
2022-03-15 18:01:52 -0300

    Número Perfeito

--- JAVA/Ex1164.java
+++ JAVA/Ex1164.java
@@ -13,16 +13,13 @@ public class Ex1164 { // Alterar o nome da classe para Main para funcionar no be
         for(int i = 0; i < numTestes; i++) {
             int n = sc.nextInt();
 
-            for(int x = 1; x < n; x++) {
-                if(n % x == 0) somaTeste += x;
-            }
+            for(int x = 1; x < n; x++) if(n % x == 0) somaTeste += x;
 
             if(somaTeste == n) System.out.printf("%d eh perfeito\n", n);
 
             else System.out.printf("%d nao eh perfeito\n", n);
 
             somaTeste = 0;
-
         }
         sc.close();
     }
