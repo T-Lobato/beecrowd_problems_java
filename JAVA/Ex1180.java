18f6f19438ebabea15ecb3f31506a6b35a2969cd
Thyago Lobato <thyagollobato@gmail.com>
2022-03-17 18:05:07 -0300

    Menor e Posição

---
+++ JAVA/Ex1180.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex1180 { // Alterar o nome da classe para Main para funcionar no beecrowd
+    public static void main(String[] args) {
+        // Objeto
+        Scanner sc = new Scanner(System.in);
+
+        // Variáveis & Entrada
+        int[] vetor = new int[sc.nextInt()];
+        int minValue = 0;
+        int position = 0;
+
+        // Processamento
+        vetor[0] = sc.nextInt();
+        minValue = vetor[0];
+        for(int i = 1; i < vetor.length; i++) {
+            vetor[i] = sc.nextInt();
+
+            if(vetor[i] < minValue) {
+            minValue = vetor[i];
+            position = i;
+            }
+            
+        }
+        System.out.printf("Menor valor: %d\n", minValue);
+        System.out.printf("Posicao: %d\n", position);
+    }
+} 
