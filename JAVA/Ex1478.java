efb8442e37d7c7be20c933af6020c399f0b9d658
Thyago Lobato <thyagollobato@gmail.com>
2022-04-01 16:42:00 -0300

    Matriz Quadrada II

--- JAVA/Ex1478.java
+++ JAVA/Ex1478.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
-public class Ex1478 {
+public class Ex1478 { // ALterar o nome da classe para Main para funcionar no beecrowd
     public static void main(String[] args) {
+        // Objeto
         Scanner sc = new Scanner(System.in);
+        
+        // Variáveis
         int valor = 0;
 
+        // Processamento / Entrada / Saída
         while (true) {
             int n = sc.nextInt();
             if (n == 0)
@@ -14,10 +18,8 @@ public class Ex1478 {
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (j >= i)
-                        valor = (j - i) + 1;
-                    else
-                        valor = (i - j) + 1;
+                    if (j >= i) valor = (j - i) + 1;
+                    else valor = (i - j) + 1;
                     mat[i][j] = valor;
                     if (j == 0) System.out.printf("%3d", mat[i][j]);
                     else System.out.printf("%4d", mat[i][j]);
