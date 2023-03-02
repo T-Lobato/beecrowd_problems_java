7a8d8dccbdcac23a40996be28d70bf9868a40be5
Thyago Lobato <thyagollobato@gmail.com>
2023-03-02 20:31:01 -0300

    LED

--- JAVA/3-String/Ex1168.java
+++ JAVA/3-String/Ex1168.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Ex1168 {
+public class Ex1168 { // Alterar o nome da classe para Main para funcionar no beecrowd
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -10,7 +10,7 @@ public class Ex1168 {
         int n = sc.nextInt();
         sc.nextLine();
         
-        for(int i = 0; i < n; i++) { // Alterar o nome da classe para Main para funcionar no beecrowd
+        for(int i = 0; i < n; i++) { 
         
         String entrada = sc.nextLine();
 
