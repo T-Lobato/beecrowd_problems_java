f0e0af6e8b84f1ce2cf07bff5fef5f0de6f21794
Thyago Lobato <thyagollobato@gmail.com>
2023-03-06 21:20:05 -0300

    Cifra de César

--- JAVA/3-String/Ex1253.java
+++ JAVA/3-String/Ex1253.java
@@ -5,29 +5,33 @@ public class Ex1253 { // Alterar o nome da classe para Main para funcionar no be
 
         Scanner sc = new Scanner(System.in);
 
-        int casosDeTeste  = sc.nextInt();
+        int casosDeTeste = sc.nextInt();
         sc.nextLine();
 
-        for(int j = 0; j < casosDeTeste; j++) {
+        for (int j = 0; j < casosDeTeste; j++) {
 
-        String entrada = sc.nextLine();
+            String entrada = sc.nextLine();
+            int chave = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println(decodeString(casosDeTeste, chave, entrada));
+
+        }
+        sc.close();
+    }
+
+    public static String decodeString(int casosDeTeste, int chave, String entrada) {
         StringBuilder stringDecodificada = new StringBuilder();
 
-        int chave = sc.nextInt();
-        sc.nextLine();
+        for (int i = 0; i < entrada.length(); i++) {
 
-        for(int i = 0; i < entrada.length(); i++) {
-
-            if((entrada.charAt(i) - chave) < 65) {
-
-                stringDecodificada.append((char)(entrada.charAt(i) - chave + 26));
-
-            } else stringDecodificada.append((char)(entrada.charAt(i) - chave));
+            if ((entrada.charAt(i) - chave) < 65) {
+                stringDecodificada.append((char) (entrada.charAt(i) - chave + 26));
+            } else
+                stringDecodificada.append((char) (entrada.charAt(i) - chave));
         }
 
-        System.out.println(stringDecodificada.toString());
+        return stringDecodificada.toString();
 
-         }
-         sc.close();
     }
 }
