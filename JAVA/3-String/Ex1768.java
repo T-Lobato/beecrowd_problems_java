fb0ba9ea5c8014f31848f10ed1e1c5c8b1845fec
Thyago Lobato <thyagollobato@gmail.com>
2023-04-05 16:35:50 -0300

    Zero vale Zero

---
+++ JAVA/3-String/Ex1768.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex1768 { // Alterar o nome da classe para Main para funcionar no beecrowd
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1, num2;
+
+        while ((num1 = sc.nextInt()) != 0 && (num2 = sc.nextInt()) != 0) {
+            String soma = String.valueOf(num1 + num2);
+            String resultado = soma.replaceAll("0", "");
+            System.out.println(resultado);
+        }
+        sc.close();
+    }
+}
